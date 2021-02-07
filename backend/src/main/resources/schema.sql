8b2d74d804c794e16730db0bd9ea442f568cc9a8
SelimHorri <horri.slim@gmail.com>
2021-02-07 16:21:43 +0100

    add schema & data files for h2

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+create table employees (
+	employee_id int not null primary key auto_increment,
+	first_name varchar(255),
+	last_name varchar(255),
+	email varchar(255),
+	job_title varchar(255),
+	phone varchar(255),
+	image_url varchar(255),
+	employee_code varchar(255)
+);
