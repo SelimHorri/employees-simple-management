aef244a70ee1c6eac5dd2647e9647c0e8088c89f
SelimHorri <horri.slim@gmail.com>
2021-02-13 15:38:57 +0100

    format queries in backend and set up things in frontend by creating modal window for crud operation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -31,4 +31,30 @@ export class AppComponent implements OnInit {
     );
   }
   
+  public onOpenModal(employee: Employee, mode: string) : void {
+    
+    const mainContainer = document.getElementById("main-container");
+    
+    const button = document.createElement("button"); // type = 'submit' by default
+    button.type = "button";
+    button.style.display = "none";
+    button.setAttribute("data-toggle", "modal");
+    
+    if (mode === "add") {
+      button.setAttribute("data-target", "#addEmployeeModal");
+    }
+    if (mode === "update") {
+      button.setAttribute("data-target", "#updateEmployeeModal");
+    }
+    if (mode === "delete") {
+      button.setAttribute("data-target", "#deleteEmployeeModal");
+    }
+    
+    mainContainer.appendChild(button);
+    button.click();
+    
+  }
+  
+  
+  
 }
